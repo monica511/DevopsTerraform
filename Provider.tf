58c217fe12a9b25508397d3839b3dca208ef879d
monica511 <75793652+monica511@users.noreply.github.com>
2023-03-29 22:58:40 +0530

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = "Monica Patil"           # this the profile which we created using aws configure cli command
+  region  = "ap-south-1"        # this will make default region as ap-south-1 which is in mumbai
+}
