f8ffe3e178642b967680c7cee18d96ef3c8e5cbe
monica511 <75793652+monica511@users.noreply.github.com>
2023-03-29 23:05:04 +0530

    Create subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,33 @@
+# public subnet
+resource "aws_subnet" "public_subnet" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.0.0/24"
+
+  availability_zone_id = "aps1-az1"
+
+  tags = {
+    Name = "public-subnet"
+  }
+
+  map_public_ip_on_launch = true
+}
+
+# private subnet
+resource "aws_subnet" "private_subnet" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "192.168.1.0/24"
+
+  availability_zone_id = "aps1-az3"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
